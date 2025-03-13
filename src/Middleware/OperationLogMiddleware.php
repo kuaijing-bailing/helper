6dba329bbdae1752db39c974128e7d60d8b929e4
jaty <jaty@shequ.life>
2025-03-13 17:03:00 +0800

    v3.1.342

--- src/Middleware/OperationLogMiddleware.php
+++ src/Middleware/OperationLogMiddleware.php
@@ -103,8 +103,16 @@ class OperationLogMiddleware implements MiddlewareInterface
         if ($operationLog['method'] == 'GET') {
             return $result;
         }
+
         // 新的固定列表查询页，不存
         if ($operationLog['method'] == 'POST' && str_ends_with($operationLog['router'], '/query')) {
+            return $result;
+        }
+
+        // 如果是不请求的URL，不存
+        $filterStr = $operationLog['method'] . ':/' . $operationLog['router'];
+        $config = config('log_report');
+        if (!empty($config) && in_array($filterStr, $config)) {
             return $result;
         }
 
