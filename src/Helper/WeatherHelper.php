5f91fed1a961908d2402acfdb180976866a85a33
jaty <jaty@shequ.life>
2025-07-16 20:01:23 +0800

    v3.1.527

--- src/Helper/WeatherHelper.php
+++ src/Helper/WeatherHelper.php
@@ -21,7 +21,7 @@ class WeatherHelper
      * @param string $city 城市简称
      * @param string $province 省份简称（最好给到，避免地址重复）
      */
-    #[Cacheable(prefix: 'getWeatherNow', ttl: 3600, listener: 'getWeatherNow-update')]
+    #[Cacheable(prefix: 'getWeatherNow', value: '#{city}_#{province}', ttl: 3600, listener: 'getWeatherNow-update')]
     public static function getWeatherNow(string $city, string $province = '')
     {
         $locationId = self::getCityLocationId($city, $province);
@@ -38,7 +38,7 @@ class WeatherHelper
             return [];
         }
 
-        $body = (string) $res->getBody(); //获取响应体，对象
+        $body = (string) $res->getBody(); // 获取响应体，对象
         $bodyArr = Json::decode($body, true);
         if ($bodyArr['code'] != '200') {
             stdLog()->error('和风天气访问失败（' . $url . '）：' . $bodyArr['code']);
@@ -49,9 +49,49 @@ class WeatherHelper
     }
 
     /**
+     * @param string $someDay 查询x天的天气
+     * @param string $city 城市简称
+     * @param string $province 省份简称（最好给到，避免地址重复）
+     *                         someDay:
+     *                         3d 3天预报。
+     *                         7d 7天预报。
+     *                         10d 10天预报。
+     *                         15d 15天预报。
+     *                         30d 30天预报。
+     */
+    #[Cacheable(prefix: 'getWeatherCondition', value: '#{someDay}_#{city}_#{province}', ttl: 3600)]
+    public static function getWeatherCondition(string $someDay, string $city, string $province = '')
+    {
+        $locationId = self::getCityLocationId($city, $province);
+        if (! $locationId) {
+            return [];
+        }
+
+        $client = new Client();
+        $url = 'https://' . cfg('qweather_dev_host') . '/v7/weather/' . $someDay . '?location=' . $locationId . '&key=' . cfg('qweather_dev_key');
+        try {
+            $res = $client->request('GET', $url);
+        } catch (GuzzleException $e) {
+            stdLog()->error('和风天气访问失败（' . $url . '）：' . $e->getMessage());
+
+            return [];
+        }
+
+        $body = (string) $res->getBody(); // 获取响应体，对象
+        $bodyArr = Json::decode($body, true);
+        if ($bodyArr['code'] != '200') {
+            stdLog()->error('和风天气访问失败（' . $url . '）：' . $bodyArr['code']);
+
+            return [];
+        }
+
+        return $bodyArr['daily'];
+    }
+
+    /**
      * 2592000 = 86400 * 30.
      */
-    #[Cacheable(prefix: 'weatherGetCityLocationId', ttl: 2592000, listener: 'weatherGetCityLocationId-update')]
+    #[Cacheable(prefix: 'weatherGetCityLocationId', value: '#{city}_#{province}', ttl: 2592000, listener: 'weatherGetCityLocationId-update')]
     public static function getCityLocationId(string $city, string $province = '')
     {
         $client = new Client();
@@ -67,7 +107,7 @@ class WeatherHelper
             return 0;
         }
 
-        $body = (string) $res->getBody(); //获取响应体，对象
+        $body = (string) $res->getBody(); // 获取响应体，对象
         $bodyArr = Json::decode($body, true);
         if ($bodyArr['code'] != '200') {
             stdLog()->error('和风天气访问失败（' . $url . '）：' . $bodyArr['code']);
